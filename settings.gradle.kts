b5d485cd9df0f7d87cc84c8f6f97701881fdedaa
Elisa Albertini <66846693+elisaalbertini@users.noreply.github.com>
2024-04-25 11:19:25 +0200

    Add hooks in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
 rootProject.name = "paguro"
 
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.23"
+}
+
+gitHooks {
+    commitMsg {
+        conventionalCommits()
+    }
+    createHooks(true)
+}
+
