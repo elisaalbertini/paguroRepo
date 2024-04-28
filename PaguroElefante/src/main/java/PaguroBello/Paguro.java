15712e56125a6312ea2994f998e5fd9812d31ca4
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2024-04-28 10:23:41 +0200

    feat: new comment

--- PaguroElefante/src/main/java/PaguroBello/Paguro.java
+++ PaguroElefante/src/main/java/PaguroBello/Paguro.java
@@ -8,5 +8,6 @@ public class Paguro {
     public String complimentaIlPaguro() {return "Che bel Paguro!";}
 
     public String insultaIlPaguro() {return "Che brutto Paguro!";}
+    //gioca con il paguro
     public String giocaConIlPaguro() {return "Giochiamo Paguro!";}
 }
