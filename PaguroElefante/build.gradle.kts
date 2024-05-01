4d6041e1f95676e06f242386d61c9364e0bbcf4e
Elisa Albertini <elisa.albertini25@gmail.com>
2024-05-01 17:58:20 +0200

    refactor: refactoring

--- PaguroElefante/build.gradle.kts
+++ PaguroElefante/build.gradle.kts
@@ -18,7 +18,7 @@ tasks.test {
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
     reports {
-        //xml.required.set(false)
+        xml.required.set(true)
         csv.required.set(true)
         //html.required.set(false)
         //csv.outputLocation.set(File("./build/reports/jacoco/test/jacocoTestReport.csv"))
