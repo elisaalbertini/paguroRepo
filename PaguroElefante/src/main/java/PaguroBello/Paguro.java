c398dc3199431fef9568ee2ce836d2faca6026bc
Elisa Albertini <elisa.albertini25@gmail.com>
2024-05-02 11:49:06 +0200

    refactor: ciao

--- PaguroElefante/src/main/java/PaguroBello/Paguro.java
+++ PaguroElefante/src/main/java/PaguroBello/Paguro.java
@@ -3,9 +3,15 @@ package PaguroBello;
 public final class Paguro {
     //funzione bellissima!
     public String salutaIlPaguro(){
+        String ciao = "ciao";
+        ciao += ciao.charAt(1);
+
         return "Ciao Paguro";
     }
-    public String complimentaIlPaguro() {return "Che bel Paguro!";}
+    public String complimentaIlPaguro() {
+        String ciao = "ciao";
+        ciao += ciao.charAt(1);
+        return "Che bel Paguro!";}
     //insulta il paguro
     public String insultaIlPaguro() {return "Che brutto Paguro!";}
     //gioca con il paguro
