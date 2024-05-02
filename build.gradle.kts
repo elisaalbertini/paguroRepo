bdf20700c3619b8f41e685b8c14942679511bfa6
Elisa Albertini <elisa.albertini25@gmail.com>
2024-05-02 16:57:59 +0200

    refactor: ciao

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
-    id("org.sonarqube") version "3.5.0.2730"
+    //id("org.sonarqube") version "3.5.0.2730"
+    //kotlin("jvm") version "1.8.21"
 }
 
 version = "1.0-SNAPSHOT"
@@ -9,7 +10,7 @@ repositories {
     mavenCentral()
 }
 
-sonar {
+/*sonar {
     properties {
         property("sonar.projectKey", "elisaalbertini_paguroRepo")
         property("sonar.organization", "elisaalbertini-pagurorepo")
@@ -25,7 +26,7 @@ subprojects {
             property("sonar.sources", "PaguroElefante/src")
         }
     }
-}
+}*/
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
