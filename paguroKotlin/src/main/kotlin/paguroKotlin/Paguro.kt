0f94847abfe75580251d00a3270a37a3cfb8738e
Elisa Albertini <elisa.albertini25@gmail.com>
2024-05-06 10:08:10 +0200

    refactor: kotlin

---
+++ paguroKotlin/src/main/kotlin/paguroKotlin/Paguro.kt
@@ -0,0 +1,15 @@
+package paguroKotlin
+
+/**
+ * Classe bella
+ */
+class Paguro {
+
+    /**
+     * Saluta il paguro
+     * @return saluto
+     */
+    fun salutaIlPaguro(): String{
+        return "Ciao Paguro!"
+    }
+}
