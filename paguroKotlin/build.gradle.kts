f4db5249a3396a7d38b80c376c83421686ccea57
Elisa Albertini <elisa.albertini25@gmail.com>
2024-05-06 11:15:54 +0200

    refactor: coverage

--- paguroKotlin/build.gradle.kts
+++ paguroKotlin/build.gradle.kts
@@ -3,10 +3,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.8.21"
     id("org.jetbrains.dokka") version "1.9.20"
+    id("jacoco")
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+jacoco {
+    toolVersion = "0.8.11"
+}
 
 repositories {
     mavenCentral()
@@ -18,6 +23,14 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 }
 
-tasks.getByName<Test>("test") {
+tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+    }
 }
