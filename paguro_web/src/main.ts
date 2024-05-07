d8add67bbfaa90fb25a2d0679553b7e00ccb8054
Elisa Albertini <elisa.albertini25@gmail.com>
2024-05-07 11:25:37 +0200

    refactor: format

--- paguro_web/src/main.ts
+++ paguro_web/src/main.ts
@@ -1,14 +1,14 @@
 const helmet = require('helmet');
 const app = helmet();
 
-export function salutaIlPaguro(): string{
+export function salutaIlPaguro(): string {
     return "Ciao paguro!";
 }
 
-export function salutaIlPaguroPersonalizzato(personalizzazione: string): string{
+export function salutaIlPaguroPersonalizzato(personalizzazione: string): string {
     return "Ciao " + personalizzazione + " paguro!";
 }
 
-export function complimentaIlPaguro(): string{
+export function complimentaIlPaguro(): string {
     return "Bel paguro!";
 }
